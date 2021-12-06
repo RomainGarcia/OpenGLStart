7defa42b85df8d098b0c8bb88dcfd525bb06a746
RomainGarcia <garcia.romain@gmail.com>
2021-12-06 22:58:59 +0100

    Fix title

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main()
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "GLFW CMake starter", NULL, NULL);
+    window = glfwCreateWindow(640, 480, "OpenGL", nullptr, nullptr);
     if (!window)
     {
         glfwTerminate();
